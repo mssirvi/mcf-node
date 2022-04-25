42613f101c70a3fa0eba30b07975df45819a2c0d
manish <ms8829@live.com>
2022-04-25 08:34:37 +0530

    added a lambda for getjson endpoint and started on this api

--- src/lib/collectdata.js
+++ src/lib/collectdata.js
@@ -5,15 +5,15 @@ import createError from "http-errors";
 const apiEndpoint = "http://restapi.adequateshop.com/api/Traveler";
 
 async function getJsonData(page) {
-    let jsondata,result;
+    let jsondata, result;
     try {
         result = await axios.get(apiEndpoint + `?page=${page}`);
     } catch (error) {
         console.log(error);
         throw new createError.InternalServerError("Something went wrong while requesting xml data");
     }
-    
-    parseString(result.data,{explicitArray: false}, function (err, result) {
+
+    parseString(result.data, { explicitArray: false }, function (err, result) {
         if (err) {
             console.log(err);
             throw new createError.InternalServerError("Something went wrong while parsing xml to json");
@@ -27,9 +27,9 @@ export async function collectFullData() {
     let fullData = [];
     let resultdata = await getJsonData(1);
     let totalPages = parseInt(resultdata.TravelerinformationResponse.total_pages);
-    console.log("here",totalPages);
+    console.log("here", totalPages);
     for (let i = 1; i <= totalPages; i++) {
-        console.log("collecting data page: ",i);
+        console.log("collecting data page: ", i);
         try {
             let pageData = await getJsonData(i);
             // console.log(pageData.TravelerinformationResponse.travelers.Travelerinformation);
@@ -39,6 +39,6 @@ export async function collectFullData() {
         }
     }
     console.log("All pages data retrieved.");
-    
-    return fullData;
+
+    return { "travelers": fullData };
 }
